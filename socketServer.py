3f4aa3ea21e9f78538416b0886343252bdb50a6b
Chris Cody <3807221+aussiechris@users.noreply.github.com>
2023-04-12 02:00:11 +1000

    server handles connection and client id

--- socketServer.py
+++ socketServer.py
@@ -6,6 +6,61 @@ DEFAULT_PORT = 4242
 RECIEVE_BUFFER_SIZE = 1024
 PRINT_CLIENT_COMMANDS = True
 PRINT_SERVER_COMMANDS = True
+PRINT_VERBOSE_STATUS = True
+
+
+# send message to connected client
+def send_message(conn, message):
+    if PRINT_SERVER_COMMANDS:
+        print(f'Server: {message}')
+    conn.sendall(message.encode())
+
+
+# recieve message from connected client
+def recieve_message(conn):
+    data = conn.recv(RECIEVE_BUFFER_SIZE)
+    if data:
+        if PRINT_CLIENT_COMMANDS:
+            print(f'Client: {data.decode()}')
+        return data.decode()
+    else:
+        return None
+
+
+# initiate client connection
+def connect_client(connected_clients):
+    try:
+        command, client_ID = recieve_message(conn).split(" ", 1)
+        if command == 'CONNECT' and connected_clients.count(client_ID) == 0:
+            connected_clients.append(client_ID)
+            response = 'CONNECT: OK'
+            if PRINT_VERBOSE_STATUS:
+                print(f'Connected client with ID: {client_ID}')
+        else:
+            response = 'CONNECT: ERROR'
+            if PRINT_VERBOSE_STATUS:
+                print(
+                    f'Error: Already connected to client with ID: {client_ID}')
+    except:
+        response = 'CONNECT: ERROR'
+        if PRINT_VERBOSE_STATUS:
+            print(
+                f'Error: Unknown error occured while connecting client')
+    return response, client_ID
+
+
+# remove client connection
+def disconnect_client(connected_clients, client_ID):
+    # TODO: check if we need to delete client ID if client disconnects unexpectedly
+    try:
+        connected_clients.remove(client_ID)
+        if PRINT_VERBOSE_STATUS:
+            print(f'Disconnected client with ID: {client_ID}')
+    except:
+        if PRINT_VERBOSE_STATUS:
+            print(
+                f'Error: Could not remove client connection record')
+
 
 print(f'Server Started')
 
@@ -14,7 +69,7 @@ parser = argparse.ArgumentParser("socketServer")
 parser.add_argument(
     "port", help="Port number for incoming connections", type=int, nargs='?', default=DEFAULT_PORT)
 args = parser.parse_args()
-
+connected_clients = []
 
 print(f'Listening on: {HOST}:{args.port}')
 while True:
@@ -25,40 +80,26 @@ while True:
         with conn:
             print(f'Connection recieved from: {addr}')
 
-            data = conn.recv(RECIEVE_BUFFER_SIZE)
-            if not data:
-                break
-
             # check for valid user ID
-            if PRINT_CLIENT_COMMANDS:
-                print(f'Client: {data.decode()}')
+            response, client_ID = connect_client(connected_clients)
+            send_message(conn, response)
 
-            command, data = data.decode().split(" ", 1)
-            if command == 'CONNECT':
-                response = 'CONNECT: OK'
+            if response != 'CONNECT: OK':
+                print(f'Connection failed. Waiting for new connection...')
             else:
-                response = 'CONNECT: ERROR'
+                # Connection established, start main loop
+                while True:
+                    data = conn.recv(RECIEVE_BUFFER_SIZE)
+                    if not data:
+                        break
+                    # handle recieved data
+                    if PRINT_CLIENT_COMMANDS:
+                        print(f'Client: {data.decode()}')
 
-            # send response back to client
-            if PRINT_CLIENT_COMMANDS:
-                print(f'Server: {response}')
+                    response = data.strip().decode()
+                    # send response back to client
+                    send_message(conn, response)
 
-            conn.sendall(response.encode())
-
-            # Connection established, start main loop
-            while True:
-                data = conn.recv(RECIEVE_BUFFER_SIZE)
-                if not data:
-                    break
-                # handle recieved data
-                if PRINT_CLIENT_COMMANDS:
-                    print(f'Client: {data.decode()}')
-
-                response = data.strip().decode()
-                # send response back to client
-                if PRINT_CLIENT_COMMANDS:
-                    print(f'Server: {response}')
-
-                conn.sendall(response.encode())
-
-            print(f'Connection lost. Waiting for new connection...')
+                # Connection lost, remove client from connected clients list
+                disconnect_client(connected_clients, client_ID)
+                print(f'Connection lost. Waiting for new connection...')
